a44fd135c3251b01a353bb4411ad072ecc380f0c
Mario <smoke_07@live.com>
2021-05-18 18:52:37 -0500

    Otros cambios en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <div class="container">
     <div class="row my-5 justify-content-center">
       <div class="col-md-9">
-        <h1>Bienvenido - Sistema de faltas</h1>
+        <h1>Bienvenido al Sistema que manejara faltas</h1>
         
       </div>
 
